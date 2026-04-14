86e22c38b6bf2c8839b5c2a0603b715f32312e0b
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2026-04-14 19:24:28 +0530

    functions

---
+++ basic_syntax/functions.java
@@ -0,0 +1,11 @@
+class functions{
+    void greet(){
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        functions obj = new functions();
+        obj.greet();
+        
+    }
+}
